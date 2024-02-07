fd0b56bb0dd74cc3ea96b283240aac216d169436
Misandifov <137529811+Misandifov@users.noreply.github.com>
2024-02-07 14:35:13 +0500

    yangilandi

--- news_project/settings.py
+++ news_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-opt$2xykw^&fpz%#=yn%&n&auj6^g4lb4w1076jq)qppwky(xt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
