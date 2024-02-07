51b73a796aaffc9666b307e852ebd3483c8c60e6
Misandifov <137529811+Misandifov@users.noreply.github.com>
2024-02-07 14:26:36 +0500

    yabgilandi

--- news_project/settings.py
+++ news_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-opt$2xykw^&fpz%#=yn%&n&auj6^g4lb4w1076jq)qppwky(xt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
